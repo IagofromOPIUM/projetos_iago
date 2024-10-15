bf77c27f0e8e89116aea31bc4c6cf8aaefc1469f
Iago Boettge <iagoboettge@gmail.com>
2024-10-15 13:44:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function formacoes(){
+    const formacoes = document.getElementsByClassName('formacoes')[0];
+    if(formacoes.style.display == 'none'){
+        formacoes.style.display = 'block';
+    } else{
+        formacoes.style.display = "none";
+    }
+
+}
+
+function conhecimentos(){
+    const conhecimentos = document.getElementsByClassName('conhecimentos')[0];
+    if(conhecimentos.style.display == 'none'){
+        conhecimentos.style.display = 'block';
+    }else{
+        conhecimentos.style.display = 'none';
+    }
+}
+
+function certificados(){
+    const certificados = document.getElementsByClassName('certificados')[0];
+    if(certificados.style.display == 'none'){
+        certificados.style.display = "block";
+    }else{
+        certificados.style.display = 'none';
+    }
+}
+
+function experiencia(){
+    const experiencia = document.getElementsByClassName('experiencia')[0];
+    if(experiencia.style.display == 'none'){
+        experiencia.style.display = 'block';
+    }else{
+        experiencia.style.display = 'none';
+    }
+}
